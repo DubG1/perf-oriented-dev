e131cc303e3d3a33cf193b6be2afb3cf70c83b53
DubG1 <georg.gas@live.at>
2024-04-22 23:01:09 +0200

    fixed flag declaration, added mv script for csv results

---
+++ solutions/sh5/mv_res.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv ../../small_samples/*.csv ./O0
+
+mv ../../larger_samples/npb_bt/*.csv ./O0
+
+mv ../../larger_samples/npb_bt/*.csv ./O0
+
+mv ../../larger_samples/ssca2/*.csv ./O0
