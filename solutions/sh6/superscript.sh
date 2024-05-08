e55893f91c52a29c482fe1aa5b2e6ea95443d49c
DubG1 <georg.gas@live.at>
2024-05-08 02:19:22 +0200

    edited scripts

--- solutions/sh6/superscript.sh
+++ solutions/sh6/superscript.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sbatch ./tiled.sh
-
+sbatch ./scripts/tiled32.sh
+sbatch ./scripts/tiled64.sh
+sbatch ./scripts/tiled128.sh
+sbatch ./scripts/tiled256.sh
+sbatch ./scripts/tiled512.sh
+sbatch ./scripts/tiled1024.sh
